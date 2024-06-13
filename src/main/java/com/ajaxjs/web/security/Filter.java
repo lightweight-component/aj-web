de49cc5960c47f24d8657480091fe89d4ca7b2bb
sp42@qq.com <sp42@qq.com>
2024-06-13 08:34:30 +0800

    ci

---
+++ src/main/java/com/ajaxjs/web/security/Filter.java
@@ -0,0 +1,106 @@
+package com.ajaxjs.web.security;
+
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.ajaxjs.web.Utils;
+
+/**
+ * 过滤器
+ *
+ */
+public class Filter {
+	/**
+	 * 正则匹配，若找到的话进行替换，或者按照 replFn 处理
+	 * 
+	 * @param str
+	 * @param p
+	 * @param replFn
+	 * @return
+	 */
+	private static String clean(String str, Pattern p, Function<Matcher, String> replFn) {
+		if (Utils.isEmtpyString(str))
+			return str;
+
+		Matcher matcher = p.matcher(str);
+
+		if (matcher.find())
+			return replFn == null ? matcher.replaceAll("") : replFn.apply(matcher);
+		else
+			return str;
+	}
+
+	private static final Pattern CRLF_Pattern = Pattern.compile("\\r|\\n");
+
+	/**
+	 * 检测 CRLF 的过滤器 又叫做 HTTP Response Splitting
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String cleanCRLF(String str) {
+		return clean(str, CRLF_Pattern, null);
+	}
+
+	/**
+	 * 侦测 SQL 脚本的正则
+	 */
+	private static final String reg = "(?:')|(?:--)|(/\\*(?:.|[\\n\\r])*?\\*/)|"
+			+ "(\\b(select|update|union|delete|insert|trancate|char|into|substr|ascii|declare|exec|count|master|into|drop|execute)\\b)";
+
+	private static final Pattern SQL_Pattern = Pattern.compile(reg, Pattern.CASE_INSENSITIVE);
+
+	/**
+	 * 检测 SQL 注入的过滤器
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String cleanSqlInject(String str) {
+		return clean(str, SQL_Pattern, null);
+	}
+
+	private static final Pattern XSS_Pattern = Pattern.compile("<script[^>]*?>.*?</script>");
+
+	/**
+	 * 对于敏感字符串该如何处理
+	 */
+	public enum Handle {
+		/**
+		 * 转义字符串
+		 */
+		TYPE_ESCAPSE,
+
+		/**
+		 * 删除字符串
+		 */
+		TYPE_DELETE
+	}
+
+	/**
+	 * XSS 过滤器
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String cleanXSS(String str) {
+		return cleanXSS(str, Handle.TYPE_DELETE);
+	}
+
+	/**
+	 * XSS 过滤器
+	 * 
+	 * @param str
+	 * @param type
+	 * @return
+	 */
+	public static String --------(String str, Handle type) {
+		return clean(str, XSS_Pattern, matcher -> {
+			if (type == Handle.TYPE_DELETE)
+				return matcher.replaceAll("");
+			else
+				return matcher.group().replace("<", "&lt;").replace(">", "&gt;");
+		});
+	}
+}
